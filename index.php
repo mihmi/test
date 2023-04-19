91959309f7af39b1e108a3f2aa91022f1352eaa1
mohamed aherdane <mohamed.aherdane@gmail.com>
2023-04-19 11:41:00 +0000

    Ajout nouveau fichier

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> page test cherry pick</title>
+    </head>
+    <body>
+        <div>
+            <p>
+                Lorem ipsum
+            </p>
+        </div>
+    </body>
+</html>
